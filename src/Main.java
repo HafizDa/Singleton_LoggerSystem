a0875db8bcb530745e7e16d04387138745283b3c
HafizDa <hafizs@metropolia.fi>
2024-02-08 23:50:04 +0200

    Done

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,17 @@ public class Main {
         // Get the instance of the logger
         Logger logger = Logger.getInstance();
 
-        // Change file name
-        logger.setFileName("new_log.txt");
 
-        // Write log messages
+        logger.setFileName("log.txt");
+
         logger.write("Simulation started");
         logger.write("Processing data...");
         logger.write("Simulation finished");
 
-        // Remember to close the logger
+
         logger.close();
 
-        // Output a message to indicate that the program has completed
+
         System.out.println("Program completed. Check the log file for messages.");
     }
 }
